25f43ffcd4360ce95ebe3e509233314bb88ebaff
waynedane <waynedane@outlook.com>
2018-10-14 22:50:01 +0800

    Update Utils.py

--- utils/Utils.py
+++ utils/Utils.py
@@ -1,1 +1,6 @@
-def
+import torch 
+import torch.nn.functional as F
+def cosinesim(keyphrases, text):
+  text = text.repeat(len(keyphrases))
+  return F.cosine_similarity(keyphrase,text, dim=1,eps=1e-06)
+  
